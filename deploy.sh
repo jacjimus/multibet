783a08d3425c168b14f02e071d0a74cb2f35a05a
James Makau <james.m@whatagraph.com>
2022-03-23 21:45:35 +0300

    changes to the deploy file

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@ set -e
 
 echo "Deploying application ..."
 
-# Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
      #git fetch --all
     git pull origin master
@@ -24,6 +22,5 @@ echo "Deploying application ..."
     # Reload PHP to update opcache
     #echo "" | sudo -S service php7.4-fpm reload
 # Exit maintenance mode
-php artisan up
 
 echo "Application deployed!"
