0934b6ae50b918ca058c7a24901757337cde3784
James Makau <james.m@whatagraph.com>
2022-03-23 21:38:46 +0300

    changes to the deploy file

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ echo "Deploying application ..."
     #composer update --no-interaction --prefer-dist --optimize-autoloader
 
     # Migrate database
-    php artisan migrate --force
+    php artisan migrate
 
     # Note: If you're using queue workers, this is the place to restart them.
     # ...
 
     # Clear cache
-    php artisan optimize --force
+    php artisan optimize
 
     # Reload PHP to update opcache
     #echo "" | sudo -S service php7.4-fpm reload
